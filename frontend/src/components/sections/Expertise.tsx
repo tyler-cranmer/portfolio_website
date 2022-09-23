3f50fa28b584f1220436f0ac331ad4a1466d8855
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-22 21:07:28 -0600

    expertise

--- frontend/src/components/sections/Expertise.tsx
+++ frontend/src/components/sections/Expertise.tsx
@@ -1,9 +1,156 @@
-import React from 'react'
+import React from 'react';
+import { IconContext } from 'react-icons/lib';
+import {
+  Box,
+  Container,
+  SvgIcon,
+  SvgIconProps,
+  Typography,
+  Paper,
+  Grid,
+} from '@mui/material';
+import theme from '../../theme';
+import { FaCogs, FaCubes, FaCode } from 'react-icons/fa';
 
-function Expertise() {
+
+function CogsIcon(props: SvgIconProps) {
   return (
-    <div>Expertise</div>
-  )
+    <IconContext.Provider value={{ className: 'cog-icons' }}>
+      <SvgIcon {...props}>
+        <FaCogs className='cog-icons' />
+      </SvgIcon>
+    </IconContext.Provider>
+  );
 }
 
-export default Expertise
+function CodeIcon(props: SvgIconProps) {
+    return (
+      <IconContext.Provider value={{ className: 'dev-icons' }}>
+        <SvgIcon {...props}>
+          <FaCode className='dev-icons' />
+        </SvgIcon>
+      </IconContext.Provider>
+    );
+}
+
+function BlockIcon(props: SvgIconProps) {
+  return (
+    <IconContext.Provider value={{ className: 'block-icons' }}>
+      <SvgIcon {...props}>
+        <FaCubes className='block-icons' />
+      </SvgIcon>
+    </IconContext.Provider>
+  );
+}
+
+type Props = {};
+
+function Expertise({}: Props) {
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.grey[900],
+        minHeight: '80vh',
+      }}>
+      <Container>
+        <Box mb={25} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Typography variant='h1' sx={{ fontWeight: 'Bold' }}>
+            My Expertise
+          </Typography>
+        </Box>
+        <Grid container columns={{ xs: 3, md: 12 }} columnSpacing={10}>
+          <Grid item xs={4}>
+            <Paper
+              elevation={0}
+              sx={{
+                padding: '70px 40px 0px',
+                maxWidth: '380px',
+                minHeight: '440px',
+                backgroundColor: 'rgb(33, 43, 54)',
+                textAlign: 'center',
+                borderRadius: '20px',
+                margin: 'auto',
+              }}>
+              <span className='spanIcon block-avmrvc'>
+                <BlockIcon
+                  sx={{
+                    height: '46px',
+                    width: '46px',
+                  }}
+                />
+              </span>
+              <Typography mb={2} variant='h6'>
+                Blockchain
+              </Typography>
+              <Typography variant='body1'>
+                Experienced in full stack development: Python, Typescript,
+                Next.js, Django, Flask
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper
+              elevation={0}
+              sx={{
+                padding: '70px 40px 0px',
+                maxWidth: '380px',
+                minHeight: '440px',
+                backgroundColor: 'rgb(33, 43, 54)',
+                textAlign: 'center',
+                borderRadius: '20px',
+                margin: 'auto',
+              }}
+              className='cardCenter css-avmrvc'>
+              <span className='spanIcon code-avmrvc'>
+                <CodeIcon
+                  sx={{
+                    height: '46px',
+                    width: '46px',
+                  }}
+                />
+              </span>
+              <Typography mb={2} variant='h6'>
+                Software Development
+              </Typography>
+              <Typography variant='body1'>
+                Experienced in full stack development: Python, Typescript,
+                Next.js, Django, Flask
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={4}>
+            <Paper
+              elevation={0}
+              sx={{
+                padding: '70px 40px 0px',
+                maxWidth: '380px',
+                minHeight: '440px',
+                backgroundColor: 'rgb(33, 43, 54)',
+                textAlign: 'center',
+                borderRadius: '20px',
+                margin: 'auto',
+              }}>
+              <span className='spanIcon cogs-avmrvc'>
+                <CogsIcon
+                  sx={{
+                    height: '46px',
+                    width: '46px',
+                  }}
+                />
+              </span>
+              <Typography mb={2} variant='h6'>
+                Machine Learning
+              </Typography>
+              <Typography variant='body1'>
+                Experienced in full stack development: Python, Typescript,
+                Next.js, Django, Flask
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+}
+
+export default Expertise;
