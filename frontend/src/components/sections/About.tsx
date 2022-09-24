0213e5c26a336a687bc19252628476b793adac5a
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-24 01:14:27 -0600

    about motion control

--- frontend/src/components/sections/About.tsx
+++ frontend/src/components/sections/About.tsx
@@ -3,8 +3,83 @@ import { Box, Container, Typography, Grid } from '@mui/material';
 import theme from '../../theme';
 import Image from 'next/image';
 import { Stack } from '@mui/system';
+import { motion, MotionConfigContext, useAnimation, Variants } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+
+const aboutVariant: Variants = {
+  offscreen: {
+    scale: 1,
+    opacity: 0,
+    y: 80,
+  },
+  offscreenPic: {
+    scale: 0,
+    opacity: 0,
+    y: 80,
+  },
+  onscreen: {
+    scale: 1,
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.9,
+    },
+  },
+  onscreenP1: {
+    scale: 1,
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1,
+    },
+  },
+  onscreenP2: {
+    scale: 1,
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1.1,
+    },
+  },
+  onscreenP3: {
+    scale: 1,
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1.2,
+    },
+  },
+  onscreenPic: {
+    scale: 1,
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: .9,
+    },
+  },
+};
 
 function About() {
+  const control = useAnimation();
+  const controlP1 = useAnimation();
+  const controlP2 = useAnimation();
+  const controlP3 = useAnimation();
+  const controlPic = useAnimation();
+
+  const [ref, inView] = useInView();
+
+    useEffect(() => {
+      if (inView) {
+        control.start('onscreen');
+        controlP1.start('onscreenP1');
+        controlP2.start('onscreenP2');
+        controlP3.start('onscreenP3');
+        controlPic.start('onscreenPic');
+      }
+    }, [control, inView]);
+  
+  
   return (
     <Box
       id='about'
@@ -14,38 +89,64 @@ function About() {
           <Box sx={{ display: 'flex' }}>
             <Box sx={{ maxWidth: '50%' }}>
               <Stack spacing={3} pr={4}>
-                <Typography
-                  variant='h2'
-                  sx={{ fontWeight: 'Bold' }}
-                  gutterBottom>
-                  About Me
-                </Typography>
-                <Typography gutterBottom>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Placeat alias quisquam unde sed vitae, error cumque eveniet
-                  qui, atque deleniti repellendus voluptates maiores, delectus
-                  soluta! Quis dolor inventore quibusdam alias?
-                </Typography>
-                <Typography gutterBottom>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Placeat alias quisquam unde sed vitae, error cumque eveniet
-                  qui, atque deleniti repellendus voluptates maiores, delectus
-                  soluta! Quis dolor inventore quibusdam alias?
-                </Typography>
-                <Typography gutterBottom>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                  magnam sint similique?
-                </Typography>
+                <motion.div
+                  ref={ref}
+                  variants={aboutVariant}
+                  initial='offscreen'
+                  animate={control}>
+                  <Typography
+                    variant='h2'
+                    sx={{ fontWeight: 'Bold' }}
+                    gutterBottom>
+                    About Me
+                  </Typography>
+                </motion.div>
+                <motion.div
+                  variants={aboutVariant}
+                  initial='offscreen'
+                  animate={controlP1}>
+                  <Typography gutterBottom>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Placeat alias quisquam unde sed vitae, error cumque eveniet
+                    qui, atque deleniti repellendus voluptates maiores, delectus
+                    soluta! Quis dolor inventore quibusdam alias?
+                  </Typography>
+                </motion.div>
+                <motion.div
+                  variants={aboutVariant}
+                  initial='offscreen'
+                  animate={controlP2}>
+                  <Typography gutterBottom>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Placeat alias quisquam unde sed vitae, error cumque eveniet
+                    qui, atque deleniti repellendus voluptates maiores, delectus
+                    soluta! Quis dolor inventore quibusdam alias?
+                  </Typography>
+                </motion.div>
+                <motion.div
+                  variants={aboutVariant}
+                  initial='offscreen'
+                  animate={controlP3}>
+                  <Typography gutterBottom>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Error magnam sint similique?
+                  </Typography>
+                </motion.div>
               </Stack>
             </Box>
-            <Box sx={{ marginLeft: '1em' }}>
-              <Image
-                alt='index logo'
-                src='/face-kid.png'
-                width={500}
-                height={500}
-              />
-            </Box>
+            <motion.div
+              variants={aboutVariant}
+              initial='offscreenPic'
+              animate={controlPic}>
+              <Box sx={{ marginLeft: '1em' }}>
+                <Image
+                  alt='index logo'
+                  src='/face-kid.png'
+                  width={500}
+                  height={500}
+                />
+              </Box>
+            </motion.div>
           </Box>
         </Container>
       </Box>
