c82e7823185e7efd33a65510f324f9dcbe168a71
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-24 16:03:44 -0600

    fixed accordian spacing

--- frontend/src/components/customMinorComponents/CustomAccordian.tsx
+++ frontend/src/components/customMinorComponents/CustomAccordian.tsx
@@ -57,125 +57,127 @@ function CustomAccordian({ inView }: Props) {
   }, [control, inView]);
 
   return (
-    <div>
+    <>
       {experienceData.experience.map((item, index) => (
-        <motion.div
-          key={index}
-          custom={index}
-          variants={expVariant}
-          initial='offscreen'
-          animate={control}>
-          <Accordion
-            square={true}
-            expanded={expanded === item.panel}
-            onChange={handleChange(item.panel)}
-            sx={{
-              backgroundColor: 'rgb(33, 43, 54)',
-              marginBottom: '.5em',
-              borderTopRightRadius: '15px',
-              borderTopLeftRadius: '15px',
-              borderBottomRightRadius: '15px',
-              borderBottomLeftRadius: '15px',
-              padding: '.25em',
-            }}>
-            <AccordionSummary
-              expandIcon={
-                <ExpandMoreIcon sx={{ color: theme.palette.primary.main }} />
-              }
-              aria-controls={item.panel + 'content'}
-              id={item.panel + 'header'}>
-              <Box
-                sx={{
-                  width: '100%',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                  paddingRight: '2em',
-                }}>
-                <Box sx={{ display: 'flex' }}>
-                  <Typography sx={{ fontWeight: 'bold' }}>
-                    {item.title}
-                  </Typography>
-                  <Typography
-                    sx={{
-                      paddingLeft: '.25em',
-                      color: theme.palette.primary.main,
-                      fontWeight: 'bold',
-                    }}>
-                    @ {item.company}
+        <div key={index}>
+          <motion.div
+            custom={index}
+            variants={expVariant}
+            initial='offscreen'
+            animate={control}>
+            <Accordion
+              square={true}
+              disableGutters={false}
+              expanded={expanded === item.panel}
+              onChange={handleChange(item.panel)}
+              sx={{
+                backgroundColor: 'rgb(33, 43, 54)',
+                marginBottom: '.5em',
+                borderTopRightRadius: '15px',
+                borderTopLeftRadius: '15px',
+                borderBottomRightRadius: '15px',
+                borderBottomLeftRadius: '15px',
+                padding: '.25em',
+              }}>
+              <AccordionSummary
+                expandIcon={
+                  <ExpandMoreIcon sx={{ color: theme.palette.primary.main }} />
+                }
+                aria-controls={item.panel + 'content'}
+                id={item.panel + 'header'}>
+                <Box
+                  sx={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    paddingRight: '2em',
+                  }}>
+                  <Box sx={{ display: 'flex' }}>
+                    <Typography sx={{ fontWeight: 'bold' }}>
+                      {item.title}
+                    </Typography>
+                    <Typography
+                      sx={{
+                        paddingLeft: '.25em',
+                        color: theme.palette.primary.main,
+                        fontWeight: 'bold',
+                      }}>
+                      @ {item.company}
+                    </Typography>
+                  </Box>
+                  <Typography sx={{ color: 'text.secondary' }}>
+                    {item.duration}
                   </Typography>
                 </Box>
-                <Typography sx={{ color: 'text.secondary' }}>
-                  {item.duration}
-                </Typography>
-              </Box>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Box>
-                <Box sx={{ display: 'flex' }}>
-                  <LocationOnIcon
-                    fontSize='small'
-                    sx={{ color: theme.palette.primary.main }}
-                  />
-                  <Typography ml={1} mr={2}>
-                    {item.location}
-                  </Typography>
-                  <Link
-                    href={item.slug}
-                    color='inherit'
-                    underline='none'
-                    sx={{ display: 'flex' }}>
-                    <LaunchIcon
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box>
+                  <Box sx={{ display: 'flex' }}>
+                    <LocationOnIcon
                       fontSize='small'
                       sx={{ color: theme.palette.primary.main }}
                     />
-                    <Typography className='websiteLink' ml={1} mr={2}>
-                      {item.website}
+                    <Typography ml={1} mr={2}>
+                      {item.location}
                     </Typography>
-                  </Link>
-                </Box>
-                <Grid container spacing={1}>
-                  <Grid item xs={9}>
-                    <List dense>
-                      {item.bullets.map((bullet, index) => (
-                        <ListItem sx={{ paddingLeft: 0 }} key={index}>
-                          <ListItemIcon sx={{ minWidth: '1.75em' }}>
-                            <ArrowRightOutlinedIcon
-                              fontSize='small'
-                              sx={{ color: theme.palette.primary.main }}
-                            />
-                          </ListItemIcon>
-                          <ListItemText
-                            sx={{ fontSize: '.25rem' }}
-                            primary={bullet}
-                          />
-                        </ListItem>
-                      ))}
-                    </List>
-                  </Grid>
-                  <Grid item xs={3}>
-                    <Box
-                      sx={{
-                        height: '100%',
-                        width: '100%',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        padding: '1em',
-                      }}>
-                      <Image
-                        alt={item.alt}
-                        src={item.logo}
-                        width='255'
-                        height='125'
+                    <Link
+                      href={item.slug}
+                      color='inherit'
+                      underline='none'
+                      sx={{ display: 'flex' }}>
+                      <LaunchIcon
+                        fontSize='small'
+                        sx={{ color: theme.palette.primary.main }}
                       />
-                    </Box>
+                      <Typography className='websiteLink' ml={1} mr={2}>
+                        {item.website}
+                      </Typography>
+                    </Link>
+                  </Box>
+                  <Grid container spacing={1}>
+                    <Grid item xs={9}>
+                      <List dense>
+                        {item.bullets.map((bullet, index) => (
+                          <ListItem sx={{ paddingLeft: 0 }} key={index}>
+                            <ListItemIcon sx={{ minWidth: '1.75em' }}>
+                              <ArrowRightOutlinedIcon
+                                fontSize='small'
+                                sx={{ color: theme.palette.primary.main }}
+                              />
+                            </ListItemIcon>
+                            <ListItemText
+                              sx={{ fontSize: '.25rem' }}
+                              primary={bullet}
+                            />
+                          </ListItem>
+                        ))}
+                      </List>
+                    </Grid>
+                    <Grid item xs={3}>
+                      <Box
+                        sx={{
+                          height: '100%',
+                          width: '100%',
+                          display: 'flex',
+                          justifyContent: 'center',
+                          padding: '1em',
+                        }}>
+                        <Image
+                          alt={item.alt}
+                          src={item.logo}
+                          width='255'
+                          height='125'
+                        />
+                      </Box>
+                    </Grid>
                   </Grid>
-                </Grid>
-              </Box>
-            </AccordionDetails>
-          </Accordion>
-        </motion.div>
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          </motion.div>
+        </div>
       ))}
-    </div>
+    </>
   );
 }
 
