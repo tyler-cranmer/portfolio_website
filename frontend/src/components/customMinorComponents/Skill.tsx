a4725fbefa92894ddda6bc7c0763741f64ded7b4
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-26 17:49:34 -0600

    abou2 and skills icons

---
+++ frontend/src/components/customMinorComponents/Skill.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Box } from '@mui/system';
+import { motion } from 'framer-motion';
+import { Typography} from '@mui/material';
+import theme from '../../theme';
+type Props = {
+  directionLeft?: boolean;
+};
+
+function Skill({ directionLeft }: Props) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        position: 'relative',
+        justifyContent: 'center',
+      }}>
+      <motion.img
+        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        src='/index-token.png'
+        className='skillsIcon'
+      />
+      <Box
+        id='1'
+        sx={{
+          position: 'absolute',
+          opacity: 0,
+          borderRadius: '9999px',
+          '&:hover': {
+            opacity: 0.8,
+            transitionDuration: '400ms',
+            transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
+            background: theme.palette.grey[300],
+          },
+          height: '8rem',
+          width: '8rem',
+        }}>
+        <Box
+          id='2'
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: '100%',
+          }}>
+          <Typography sx={{ color: 'black', fontWeight: 'bold', opacity: 1 }}>
+            100%
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Skill;
---
+++ frontend/src/components/sections/About2.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Box, Container, Typography, Grid } from '@mui/material';
+import theme from '../../theme';
+import Skill from '../customMinorComponents/Skill';
+type Props = {};
+
+function About2({}: Props) {
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.grey[900],
+        overFlow: 'hidden',
+        position: 'relative',
+      }}>
+      <Box>
+        <Container>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}>
+            <Typography variant='h2' sx={{ fontWeight: 'Bold' }} gutterBottom>
+              About
+            </Typography>
+            <Container maxWidth='md'>
+              <Typography gutterBottom>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+                minus cumque pariatur veritatis sit sequi ipsa ipsam, ratione
+                itaque ad, sint velit praesentium voluptas, ex minima voluptate
+                provident in animi?
+              </Typography>
+            </Container>
+            <Container maxWidth='sm'>
+              <Box mt={3} sx={{ flexGrow: 1 }}>
+                <Grid
+                  container
+                  rowSpacing={2}
+                  columnSpacing={2}
+                  columns={{ xs: 4, sm: 8, md: 12 }}>
+                  {Array.from(Array(4)).map((_, index) => (
+                    <Grid
+                      item
+                      xs={1}
+                      sm={2}
+                      md={3}
+                      key={index}
+                      sx={{ alignItem: 'center' }}>
+                      <Skill />
+                    </Grid>
+                  ))}
+                  {Array.from(Array(4)).map((_, index) => (
+                    <Grid
+                      item
+                      xs={1}
+                      sm={2}
+                      md={3}
+                      key={index}
+                      sx={{ alignItem: 'center' }}>
+                      <Skill directionLeft />
+                    </Grid>
+                  ))}
+                  {Array.from(Array(4)).map((_, index) => (
+                    <Grid
+                      item
+                      xs={1}
+                      sm={2}
+                      md={3}
+                      key={index}
+                      sx={{ alignItem: 'center' }}>
+                      <Skill  />
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
+            </Container>
+          </Box>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
+
+export default About2;
