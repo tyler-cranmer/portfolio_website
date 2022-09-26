d66e62a805df667f1d862bacb3bfb12a854d6218
tyler-cranmer <tyler.cranmer@outlook.com>
2022-09-26 00:19:02 -0600

    fixed spacing for accordian

--- frontend/src/components/customMinorComponents/CustomAccordian.tsx
+++ frontend/src/components/customMinorComponents/CustomAccordian.tsx
@@ -57,26 +57,28 @@ function CustomAccordian({ inView }: Props) {
   }, [control, inView]);
 
   return (
-    <>
-      {experienceData.experience.map((item, index) => (
-        <div key={index}>
+
+      <div>
+        {experienceData.experience.map((item, index) => (
           <motion.div
+            className='space'
+            key={index}
             custom={index}
             variants={expVariant}
             initial='offscreen'
             animate={control}>
-            <Accordion
+            <Accordion 
               square={true}
               expanded={expanded === item.panel}
               onChange={handleChange(item.panel)}
               sx={{
                 backgroundColor: 'rgb(33, 43, 54)',
-                marginBottom: '.5em',
                 borderTopRightRadius: '15px',
                 borderTopLeftRadius: '15px',
                 borderBottomRightRadius: '15px',
                 borderBottomLeftRadius: '15px',
                 padding: '.25em',
+              
               }}>
               <AccordionSummary
                 expandIcon={
@@ -109,7 +111,7 @@ function CustomAccordian({ inView }: Props) {
                   </Typography>
                 </Box>
               </AccordionSummary>
-              <AccordionDetails >
+              <AccordionDetails>
                 <Box>
                   <Box sx={{ display: 'flex' }}>
                     <LocationOnIcon
@@ -174,10 +176,134 @@ function CustomAccordian({ inView }: Props) {
               </AccordionDetails>
             </Accordion>
           </motion.div>
-        </div>
-      ))}
-    </>
+        ))}
+      </div>
   );
+
+
+
+  // return (
+  //   <>
+  //     {experienceData.experience.map((item, index) => (
+  //       <div key={index}>
+  //         <motion.div
+  //           custom={index}
+  //           variants={expVariant}
+  //           initial='offscreen'
+  //           animate={control}>
+  //           <Accordion
+  //             square={true}
+  //             expanded={expanded === item.panel}
+  //             onChange={handleChange(item.panel)}
+  //             sx={{
+  //               backgroundColor: 'rgb(33, 43, 54)',
+  //               marginBottom: '.5em',
+  //               borderTopRightRadius: '15px',
+  //               borderTopLeftRadius: '15px',
+  //               borderBottomRightRadius: '15px',
+  //               borderBottomLeftRadius: '15px',
+  //               padding: '.25em',
+  //             }}>
+  //             <AccordionSummary
+  //               expandIcon={
+  //                 <ExpandMoreIcon sx={{ color: theme.palette.primary.main }} />
+  //               }
+  //               aria-controls={item.panel + 'content'}
+  //               id={item.panel + 'header'}>
+  //               <Box
+  //                 sx={{
+  //                   width: '100%',
+  //                   display: 'flex',
+  //                   justifyContent: 'space-between',
+  //                   paddingRight: '2em',
+  //                 }}>
+  //                 <Box sx={{ display: 'flex' }}>
+  //                   <Typography sx={{ fontWeight: 'bold' }}>
+  //                     {item.title}
+  //                   </Typography>
+  //                   <Typography
+  //                     sx={{
+  //                       paddingLeft: '.25em',
+  //                       color: theme.palette.primary.main,
+  //                       fontWeight: 'bold',
+  //                     }}>
+  //                     @ {item.company}
+  //                   </Typography>
+  //                 </Box>
+  //                 <Typography sx={{ color: 'text.secondary' }}>
+  //                   {item.duration}
+  //                 </Typography>
+  //               </Box>
+  //             </AccordionSummary>
+  //             <AccordionDetails >
+  //               <Box>
+  //                 <Box sx={{ display: 'flex' }}>
+  //                   <LocationOnIcon
+  //                     fontSize='small'
+  //                     sx={{ color: theme.palette.primary.main }}
+  //                   />
+  //                   <Typography ml={1} mr={2}>
+  //                     {item.location}
+  //                   </Typography>
+  //                   <Link
+  //                     href={item.slug}
+  //                     color='inherit'
+  //                     underline='none'
+  //                     sx={{ display: 'flex' }}>
+  //                     <LaunchIcon
+  //                       fontSize='small'
+  //                       sx={{ color: theme.palette.primary.main }}
+  //                     />
+  //                     <Typography className='websiteLink' ml={1} mr={2}>
+  //                       {item.website}
+  //                     </Typography>
+  //                   </Link>
+  //                 </Box>
+  //                 <Grid container spacing={1}>
+  //                   <Grid item xs={9}>
+  //                     <List dense>
+  //                       {item.bullets.map((bullet, index) => (
+  //                         <ListItem sx={{ paddingLeft: 0 }} key={index}>
+  //                           <ListItemIcon sx={{ minWidth: '1.75em' }}>
+  //                             <ArrowRightOutlinedIcon
+  //                               fontSize='small'
+  //                               sx={{ color: theme.palette.primary.main }}
+  //                             />
+  //                           </ListItemIcon>
+  //                           <ListItemText
+  //                             sx={{ fontSize: '.25rem' }}
+  //                             primary={bullet}
+  //                           />
+  //                         </ListItem>
+  //                       ))}
+  //                     </List>
+  //                   </Grid>
+  //                   <Grid item xs={3}>
+  //                     <Box
+  //                       sx={{
+  //                         height: '100%',
+  //                         width: '100%',
+  //                         display: 'flex',
+  //                         justifyContent: 'center',
+  //                         padding: '1em',
+  //                       }}>
+  //                       <Image
+  //                         alt={item.alt}
+  //                         src={item.logo}
+  //                         width='255'
+  //                         height='125'
+  //                       />
+  //                     </Box>
+  //                   </Grid>
+  //                 </Grid>
+  //               </Box>
+  //             </AccordionDetails>
+  //           </Accordion>
+  //         </motion.div>
+  //       </div>
+  //     ))}
+  //   </>
+  // );
 }
 
 export default CustomAccordian;
