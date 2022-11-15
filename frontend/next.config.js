28135d6053646b1564923157def4f5cfdf40190a
tyler-cranmer <tyler.cranmer@outlook.com>
2022-11-14 21:05:36 -0500

    image

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,24 +1,23 @@
 /** @type {import('next').NextConfig} */
 
+// module.exports = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: [process.env.BACKEND_HOST],
+//   },
+
+// };
+
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [process.env.BACKEND_HOST],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'django-portolio-assets-teewhy.s3.amazonaws.com',
+        port: '',
+        pathname: '/media/**',
+      },
+    ],
   },
-
 };
-
-
-
-// module.exports = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'example.com',
-//         port: '',
-//         pathname: '/account123/**',
-//       },
-//     ],
-//   },
-// };
